089b20969adada524ded8681a811c8632adeba9c
Figueroa1973 <interacciones.ecologicas@gmail.com>
2025-05-19 20:03:01 -0600

    homeprofesor

---
+++ src/java/Profesor/SolicitudesProximas.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Profesor;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelos.Profesor;
+
+/**
+ *
+ * @author carlo
+ */
+public class SolicitudesProximas extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html lang='es'>");
+            out.println("<head>");
+            out.println("    <meta charset='UTF-8'>");
+            out.println("    <meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+            out.println("    <title>Solicitar Asesoría</title>");
+            out.println("    <link rel='stylesheet' href='estilos.css'>");
+            out.println("</head>");
+            out.println("<body>");
+            Profesor profesor = (Profesor) request.getSession().getAttribute("profesor");
+            if (profesor == null) {
+                response.sendRedirect("login/iniciarsesion.jsp");
+                return;
+            }
+            out.println("    <div class='titulo-bienvenida'>BIENVENIDO: <span style='border-bottom:2px solid #fff; padding:0 2rem;'>" + profesor.getNombre() + "</span></div>");
+            out.println("    <header class='header'>");
+            out.println("        <img src='imagenes/logo.png' alt='Logo' class='logo' style='height:60px;'>");
+            out.println("        <nav class='nav'>");
+            out.println("            <a href='homeprofesorr' class='nav-link'>HOME</a>");
+            out.println("            <a href='VerSolicitudes' class='nav-link nav-link-activo'> VER SOLICITUDES </a>");
+            out.println("            <a href='SolicitudesProximas' class='nav-link'>SOLICITUDES PROXIMAS</a>");
+            out.println("            <a href='login/iniciarsesion.jsp' class='nav-link cerrar-sesion'>CERRAR SESIÓN</a>");
+            out.println("        </nav>");
+            out.println("    </header>");
+            
+            out.println("    <footer class='footer'>");
+            out.println("        &copy; 2025 Asesorías Web | Mentorízate");
+            out.println("    </footer>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+}
